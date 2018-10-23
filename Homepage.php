4068286d8f0d3d98cc3824732a9cd2441b093903
Jess O'Donnell <42630169+jessdilla23@users.noreply.github.com>
2018-10-23 16:09:13 -0400

    Update Homepage.php

--- Homepage.php
+++ Homepage.php
@@ -2,11 +2,10 @@
   
 <body>
   
-  Welcome <?php echo $_POST["name"]; ?><br>
-  Your E-mail address is: <?php echo $_POST["email"];<br>
-  Your Subject is<?php? echo $POST["subject"]?>
+  Welcome <?php echo $_POST["name"];?><br>
+  Your E-mail address is: <?php echo $_POST["email"];?><br>
+  Your Subject is<?php? echo $POST["subject"]?><br>
   Your Message is<?php? echo $POST["message"]?>
-  >>>>>>> 1681ea137b638d82276ed0871a23abc9735444e7
 
 </body>
   
